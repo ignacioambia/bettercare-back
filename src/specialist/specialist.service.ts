e4be8957ac6cfa89490874a0165a0c4f98c8d68c
Ignacio Ambia <ignacio.ambia@gmail.com>
2024-01-26 12:09:51 -0600

    feat: add specilist authorization

--- src/specialist/specialist.service.ts
+++ src/specialist/specialist.service.ts
@@ -2,34 +2,30 @@ import { Injectable } from '@nestjs/common';
 import { CreateSpecialistDto } from './dto/create-specialist.dto';
 import { UpdateSpecialistDto } from './dto/update-specialist.dto';
 import { InjectModel } from '@nestjs/mongoose';
-import { Specialist } from './specialist.schema';
+import { Specialist, SpecialistDocument } from './specialist.schema';
 import { Model } from 'mongoose';
+import * as bcrypt from 'bcrypt';
 
 export type Patient = any;
 
 @Injectable()
 export class SpecialistService {
-  constructor(@InjectModel(Specialist.name) private specialistModel: Model<Specialist>){}
+  constructor(
+    @InjectModel(Specialist.name) private specialistModel: Model<Specialist>,
+  ) {}
 
-  private readonly users = [
-    {
-      userId: 1,
-      username: 'john',
-      password: 'changeme',
-    },
-    {
-      userId: 2,
-      username: 'maria',
-      password: 'guess',
-    },
-  ];
-
-  async findOne(username: string): Promise<Patient | undefined> {
-    return this.users.find((user) => user.username === username);
+  async getByEmail(email: string): Promise<SpecialistDocument | null> {
+    return this.specialistModel.findOne({ email });
   }
 
-  create(createSpecialistDto: CreateSpecialistDto) {
-    const createdSpecialist = new this.specialistModel(createSpecialistDto);
+  async create(createSpecialistDto: CreateSpecialistDto) {
+    const salt = await bcrypt.genSalt(10);
+    const passwordHash = await bcrypt.hash(createSpecialistDto.password, salt);
+
+    const createdSpecialist = new this.specialistModel({
+      ...createSpecialistDto,
+      passwordHash,
+    });
     return createdSpecialist.save();
   }
 
