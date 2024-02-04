c0053ca20b5d9e15f0b1025a26e97463f25250aa
ignacio-ambia <ignacio.ambia@gmail.com>
2024-02-04 11:48:46 -0600

    feat(config): read port from environment variables

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ async function bootstrap() {
   app.useGlobalPipes(
     new ValidationPipe({ forbidNonWhitelisted: true, whitelist: true }),
   );
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
 }
 bootstrap();
